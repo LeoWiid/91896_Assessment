c5a16c6141f2d6f69afe234c183e35465cdfde06
LeoWiid <139720063+LeoWiid@users.noreply.github.com>
2024-08-21 21:19:01 +1200

    Add files via upload

--- 05_string_checker_v1.py
+++ 05_string_checker_v1.py
@@ -1,11 +1,17 @@
-def string_checker(question, num_letters, valid_responses):
-    error = "Please choose {} or {}".format(valid_responses[0],
-                                            valid_responses[1])
-
+def string_checker(question, valid_responses):
     while True:
-
+        error = f"Please enter a valid response from {valid_responses}"
         response = input(question).lower()
 
         for item in valid_responses:
-            if response == item[:num_letters] or response == item:
+            if item == response or response == item[:2]:
                 return item
+        print(error)
+
+#Main Routine
+
+payment_option = ["cash", "credit"]
+
+payment_method = string_checker("How would you like to pay (cash/credit)? ", payment_option)
+
+print(f"Thank you! Your order has been confirmed. You will pay with {payment_method}.")
